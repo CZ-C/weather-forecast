e2fe6a0d0c8e604ab45797b0c38b576424e2a80d
Chuanzi Qin <cqin@loc8.com>
2019-09-23 14:43:52 +1000

    finished

--- app.js
+++ app.js
@@ -19,10 +19,31 @@ app.config(function ($routeProvider) {
         });
 });
 
-app.controller('homeController', ['$scope', '$log', function ($scope, $log) {
+app.service('nameService', function () {
+
+    let self = this;
+    self.city = 'London';
+
+
+});
+
+app.controller('homeController', ['$scope', '$log', 'nameService', function ($scope, $log, nameService) {
+
+    $scope.cityName = nameService.city;
+    $scope.$watch('cityName', function () {
+        nameService.city = $scope.cityName;
+    });
 
 }]);
 
-app.controller('forecastController', ['$scope', '$log', function ($scope, $log) {
+app.controller('forecastController', ['$scope', '$log', 'nameService', '$resource', function ($scope, $log, nameService, $resource) {
+
+    $scope.cityName = nameService.city;
+
+    $scope.weatherAPI = $resource('https://samples.openweathermap.org/data/2.5/forecast/daily?id=524901&appid=b1b15e88fa797225412429c1c50c122a1');
+
+    $scope.result = $scope.weatherAPI.get();
+
+    console.log($scope.result);
 
 }]);
